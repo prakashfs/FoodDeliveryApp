277e2069b5696f7d68705c7d3e4b85adaf37e9fd
Prakash Sengirayar <prakash.fs@gmail.com>
2021-08-25 00:35:40 +0800

    Refactor redundant code of collection view cell model creation

--- FoodDeliveryApp/ViewModel/FoodDashboardViewModel.swift
+++ FoodDeliveryApp/ViewModel/FoodDashboardViewModel.swift
@@ -28,6 +28,16 @@ class FoodDashboardViewModel {
         self.viewType = type
     }
     
+    fileprivate func convertToModel(_ items: [Item]) -> [FoodCellModel] {
+        return items.map { FoodCellModel(image: UIImage(named: $0.image),
+                                         imageName: $0.image,
+                                         name: $0.name,
+                                         details: $0.itemDescription,
+                                         sizeInGrams: $0.weight,
+                                         sizeInCm: $0.size,
+                                         price: Double($0.price)) }
+    }
+    
     func loadFoodFromAPI(_ filter: FoodType) {
         foodsAPI.loadFoods { [weak self] response in
             guard let self = self else { return }
@@ -37,13 +47,7 @@ class FoodDashboardViewModel {
                 for food in response.foods {
                     if food.categoryCode == filter.rawValue {
                         let items = food.items
-                        let pizzaItems = items.map { FoodCellModel(image: UIImage(named: $0.image),
-                                                                   imageName: $0.image,
-                                                                   name: $0.name,
-                                                                   details: $0.itemDescription,
-                                                                   sizeInGrams: $0.weight,
-                                                                   sizeInCm: $0.size,
-                                                                   price: Double($0.price)) }
+                        let pizzaItems = self.convertToModel(items)
                         self.parsedAPIresponse.accept(pizzaItems)
                     }
                 }
@@ -59,13 +63,7 @@ class FoodDashboardViewModel {
             for food in cachedResponse.foods {
                 if food.categoryCode == forType.rawValue {
                     let items = food.items
-                    let pizzaItems = items.map { FoodCellModel(image: UIImage(named: $0.image),
-                                                               imageName: $0.image,
-                                                               name: $0.name,
-                                                               details: $0.itemDescription,
-                                                               sizeInGrams: $0.weight,
-                                                               sizeInCm: $0.size,
-                                                               price: Double($0.price)) }
+                    let pizzaItems = self.convertToModel(items)
                     self.parsedAPIresponse.accept(pizzaItems)
                 }
             }
